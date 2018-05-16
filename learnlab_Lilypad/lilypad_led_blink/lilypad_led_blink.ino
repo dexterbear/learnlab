f2a3c31e8f1a33f75b03ae9226199d428ae5e4b8
learnlab <39142544+dexterbear@users.noreply.github.com>
2018-05-16 20:56:07 +0900

    Update lilypad_led_blink.ino

--- learnlab_Lilypad/lilypad_led_blink/lilypad_led_blink.ino
+++ learnlab_Lilypad/lilypad_led_blink/lilypad_led_blink.ino
@@ -1,13 +1,11 @@
 int led_1 = 5;
-int led_2 = 6;
-int led_3 = 7;
+
 
 void setup() {
   Serial.begin(9600);
   
   pinMode(led_1,OUTPUT);
-  pinMode(led_2,OUTPUT);
-  pinMode(led_3,OUTPUT);
+
 }
 
 void loop() {
@@ -15,14 +13,6 @@ void loop() {
   delay(100);
   digitalWrite(led_1,LOW);
   delay(100);
-  digitalWrite(led_2,HIGH);
-  delay(100);
-  digitalWrite(led_2,LOW);
-  delay(100);
-  digitalWrite(led_3,HIGH);
-  delay(100);
-  digitalWrite(led_3,LOW);
-  delay(200);
 }
   
 
