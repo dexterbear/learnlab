4f2ea28be5926b6717c432371dc0a67c6b0877ca
learnlab <39142544+dexterbear@users.noreply.github.com>
2018-05-17 10:05:25 +0900

    Update lilypad_temperature.ino

--- learnlab_Lilypad/lilypad_temperature/lilypad_temperature.ino
+++ learnlab_Lilypad/lilypad_temperature/lilypad_temperature.ino
@@ -8,8 +8,9 @@ char c;
 void setup() {
   Serial.begin(9600);
   pinMode(temper_sensor,INPUT);
-  
-  pinMode(A5,OUTPUT);
+
+  pinMode(3,OUTPUT);
+  pinMode(A5,OUTPUT);   //온도센서의 (+)극을 아날로그핀에 
   digitalWrite(A5,HIGH);
 
   
@@ -18,15 +19,24 @@ void setup() {
 void loop() {
   temper_read = analogRead(temper_sensor);
   temper_voltage = temper_read * 3.3 / 1023.0;
-  temper_c = (temper_voltageage - 0.5)*100; //섭시온도
-  temper_f = (temper*9/5)+32; //화씨온도
+  temper_c = (temper_voltage - 0.5)*100; //섭시온도
+  temper_f = (temper_c*9/5)+32; //화씨온도
   Serial.print("temperature_c:");
   Serial.println(temper_c);
   Serial.print("temperature_f:");
   Serial.println(temper_f);
+
+  if(temper_c >= 25){
+    digitalWrite(3,1);
+    delay(5);
+  }
+
+  else if(temper_c < 24){
+    digitalWrite(3,0);
+    delay(5);
+  }
 }
   
 
 
   
-
